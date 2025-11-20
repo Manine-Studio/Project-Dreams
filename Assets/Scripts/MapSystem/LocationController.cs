3fd03b0d22ed4e27c2ef8fafd2c924d520b688e1
Alien089 <gabri.moratelli@gmail.com>
2025-11-21 00:32:46 +0100

    initial work on location controller

---
+++ Assets/Scripts/MapSystem/LocationController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace MapSystem
+{
+    public class LocationController : MonoBehaviour
+    {
+        [SerializeField] private Material _xEnableMaterial;
+        [SerializeField] private Material _xDisbleMaterial;
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
