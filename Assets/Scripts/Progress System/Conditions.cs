574a5edaa8c1ce4b85a2b41b32444cf5d4c1bac3
Muso <c_acanfora@hotmail.it>
2026-01-15 14:19:10 +0100

    Updated dialogues overall

--- Assets/Scripts/Progress System/Conditions.cs
+++ Assets/Scripts/Progress System/Conditions.cs
@@ -3,5 +3,13 @@ public enum Conditions
     None,
     Key,
     Test,
-	PassaggioScena
+	DayOne,
+	DayOne_ArguingChoice,
+	DayOne_TavernFirstQuestion,
+	DayOne_TavernSecondQuestion,
+	DayOne_TavernThirdQuestion,
+	DayOne_PortalCheck,
+	DayOne_AltarCheck,
+	DayOne_WallCheck,
+	DayOne_UldonSpoken
 }
